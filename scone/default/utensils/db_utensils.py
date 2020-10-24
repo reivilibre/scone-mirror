11a62d00234bc166d0d27e176afec67579234b53
reivilibre <38398653+reivilibre@users.noreply.github.com>
2020-10-24 13:34:28 +0100

    Postgres fixes. Much more robust and actually usable now. (#6)

--- scone/default/utensils/db_utensils.py
+++ scone/default/utensils/db_utensils.py
@@ -1,28 +1,60 @@
+import logging
+
 import attr
+
+try:
+    import asyncpg
+except ImportError:
+    asyncpg = None
 
 from scone.common.chanpro import Channel
 from scone.sous import Utensil
 from scone.sous.utensils import Worktop
+
+logger = logging.getLogger(__name__)
+
+if not asyncpg:
+    logger.info("asyncpg not found, install if you need Postgres support")
 
 
 @attr.s(auto_attribs=True)
 class PostgresTransaction(Utensil):
     database: str
 
-    async def execute(self, channel: Channel, worktop: Worktop) -> None:
-        import asyncpg
+    # statements like CREATE DATABASE are not permitted in transactions.
+    use_transaction_block: bool = True
 
-        conn = await asyncpg.connect(database=self.database)
-        try:
-            async with conn.transaction():
-                while True:
-                    query, *args = await channel.recv()
-                    if query is None:
-                        break
+    async def execute(self, channel: Channel, worktop: Worktop) -> None:
+        if not asyncpg:
+            raise RuntimeError("asyncpg is not installed.")
+
+        async def queryloop():
+            while True:
+                next_input = await channel.recv()
+                if next_input is None:
+                    return
+                query, *args = next_input
+                if query is None:
+                    break
+                try:
                     results = [
                         dict(record) for record in await conn.fetch(query, *args)
                     ]
+                except asyncpg.PostgresError:
+                    logger.error(
+                        "Failed query %s with args %r", query, args, exc_info=True
+                    )
+                    await channel.close("Query error")
+                    raise
 
-                    await channel.send(results)
+                await channel.send(results)
+
+        conn = await asyncpg.connect(database=self.database)
+        try:
+            if self.use_transaction_block:
+                async with conn.transaction():
+                    await queryloop()
+            else:
+                await queryloop()
         finally:
             await conn.close()
